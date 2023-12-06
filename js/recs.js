8e3390482d69e302f669672b43c7a90501493454
srinisandhya539 <143665607+srinisandhya539@users.noreply.github.com>
2023-12-05 20:39:16 -0500

    Update recs.js

---
+++ js/recs.js
@@ -0,0 +1,40 @@
+document.querySelector("#loneliness").addEventListener("click", function() {
+    window.location.href = "https://www.goodreads.com/book/show/35456277-the-end-of-loneliness?ac=1&from_search=true&qid=iS8BC67UkU&rank=1";
+});
+
+document.querySelector("#suitable").addEventListener("click", function() {
+    window.location.href = "https://www.goodreads.com/book/show/50365.A_Suitable_Boy?ref=nav_sb_ss_1_9";
+});
+
+document.querySelector("#midnight").addEventListener("click", function() {
+    window.location.href = "https://www.goodreads.com/book/show/52578297-the-midnight-library?ref=nav_sb_ss_1_9";
+});
+
+document.querySelector("#anxious").addEventListener("click", function() {
+    window.location.href = "https://www.goodreads.com/book/show/52578297-the-midnight-library?ref=nav_sb_ss_1_9";
+});
+
+document.querySelector("#brotherless").addEventListener("click", function() {
+    window.location.href = "https://www.goodreads.com/book/show/60324341-brotherless-night?from_search=true&from_srp=true&qid=0Wbv2pcYEK&rank=1";
+});
+
+document.querySelector("#everlasting").addEventListener("click", function() {
+    window.location.href = "https://www.goodreads.com/book/show/59808042-the-book-of-everlasting-things?ref=nav_sb_ss_1_23";
+});
+
+document.querySelector("#theory").addEventListener("click", function() {
+    console.log("going to external page")
+    window.location.href = "https://www.youtube.com/watch?v=Salz7uGp72c";
+});
+
+document.querySelector("#ladyvan").addEventListener("click", function() {
+    console.log("going to external page");
+    window.location.href = "https://www.youtube.com/watch?v=OA8tMziteZM";
+});
+
+
+
+
+
+
+
